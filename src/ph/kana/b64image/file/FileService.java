d516e8e95cc7e48ff1e3b6a0f4789bc01490b1e6
kana0011 <marklynoel.alvarez@synacy.com>
2017-03-13 15:22:33 +0800

    Remove restriction on file formats
    Signed-off-by: kana0011 <marklynoel.alvarez@synacy.com>

--- src/ph/kana/b64image/file/FileService.java
+++ src/ph/kana/b64image/file/FileService.java
@@ -1,47 +1,19 @@
 package ph.kana.b64image.file;
 
-import org.apache.tika.Tika;
+import org.apache.tika.config.TikaConfig;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.mime.MediaType;
+import org.apache.tika.mime.MimeType;
+import org.apache.tika.mime.MimeTypeException;
+import org.apache.tika.mime.MimeTypes;
 
 import java.awt.*;
 import java.io.*;
 import java.net.URI;
 import java.nio.file.Files;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
 
 public class FileService {
 	private static FileService instance = new FileService();
-
-	private static final Map<String, String> SUPPORTED_TYPES = new HashMap<>();
-	static {
-		SUPPORTED_TYPES.put("image/jpeg", "jpg");
-		SUPPORTED_TYPES.put("image/png", "png");
-		SUPPORTED_TYPES.put("image/bmp", "bmp");
-		SUPPORTED_TYPES.put("image/gif", "gif");
-		SUPPORTED_TYPES.put("image/tiff", "tiff");
-
-		SUPPORTED_TYPES.put("application/pdf", "pdf");
-		SUPPORTED_TYPES.put("application/rtf", "rtf");
-		SUPPORTED_TYPES.put("text/plain", "txt");
-
-		SUPPORTED_TYPES.put("text/html", "html");
-		SUPPORTED_TYPES.put("application/xhtml+xml", "html");
-
-		SUPPORTED_TYPES.put("application/postscript", "ps");
-
-		SUPPORTED_TYPES.put("application/msword", "doc");
-		SUPPORTED_TYPES.put("application/x-msword", "doc");
-		SUPPORTED_TYPES.put("application/vnd.openxmlformats-officedocument.wordprocessingml.document", "docx");
-
-		SUPPORTED_TYPES.put("application/msexcel", "xls");
-		SUPPORTED_TYPES.put("application/vnd.ms-excel", "xls");
-		SUPPORTED_TYPES.put("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "xlsx");
-
-		SUPPORTED_TYPES.put("application/vnd.ms-powerpoint", "ppt");
-		SUPPORTED_TYPES.put("application/vnd.openxmlformats-officedocument.presentationml.presentation", "pptx");
-	}
 
 	public static FileService getInstance() {
 		return instance;
@@ -50,17 +22,21 @@ public class FileService {
 	private FileService() {	}
 
 	public File createTempFile(InputStream inputStream) throws FileOperationException {
-		Tika tika = new Tika();
+		MimeTypes mimeRepository = TikaConfig
+			.getDefaultConfig()
+			.getMimeRepository();
 		try {
-			String contentType = tika.detect(inputStream);
-			validateContentType(contentType);
+			MediaType mediaType = mimeRepository
+				.detect(inputStream, new Metadata());
+			MimeType mimeType = mimeRepository
+				.forName(mediaType.toString());
 
-			String extension = "." + SUPPORTED_TYPES.get(contentType);
+			String extension = mimeType.getExtension();
 			File file = File.createTempFile("ivb64-", extension);
 			file.deleteOnExit();
 
 			return writeToFile(file, inputStream);
-		} catch (IOException e) {
+		} catch (MimeTypeException | IOException e) {
 			throw new FileOperationException("Failed to parse Base64", e);
 		}
 	}
@@ -90,12 +66,6 @@ public class FileService {
 			return Files.readAllBytes(file.toPath());
 		} catch (IOException e) {
 			throw new FileOperationException("Unable to convert file.", e);
-		}
-	}
-
-	private void validateContentType(String contentType) throws FileOperationException {
-		if (!SUPPORTED_TYPES.containsKey(contentType)) {
-			throw new FileOperationException("Base64 parsed to an unsupported file: " + contentType);
 		}
 	}
 
